111a45559ca1a39741c150be853aeef07a1454b2
freedomzx <lejongoco@gmail.com>
2021-03-08 22:58:34 -0500

    generate environment

---
+++ environmentGenerator.py
@@ -0,0 +1,76 @@
+import random
+import pygame
+
+#check if an index is within the matrix
+def checkValidIndex(d, row, col):
+    return (row >= 0 and row < d) and (col >= 0 and col < d)
+    
+#check if an index is a mine
+def checkMine(d, row, col, matrix):
+    return checkValidIndex(d, row, col) and matrix[row][col] == "mine"
+
+#generate a d x d environment with n mines in it
+def generate(d, n):
+    minesRemaining = n
+    toReturn = []
+    for i in range(d):
+        addLine = []
+        for j in range(d):
+            addLine.append(0)
+        toReturn.append(addLine)
+
+
+    cease = False
+    while minesRemaining > 0:
+        if cease:
+            break
+        for i in range(d):
+            if cease:
+                break
+            for j in range(d):
+                if toReturn[i][j] != "mine" and random.randint(1, 100) <= 5:
+                    #fill in mines by chance, update numbers of indices surrounding the mine
+                    toReturn[i][j] = "mine"
+                    #northwest
+                    if checkValidIndex(d, i-1, j-1) and toReturn[i-1][j-1] != "mine":
+                        toReturn[i-1][j-1]+=1
+                    #west
+                    if checkValidIndex(d, i, j-1) and toReturn[i][j-1] != "mine":
+                        toReturn[i][j-1]+=1
+                    #southwest
+                    if checkValidIndex(d, i+1, j-1) and toReturn[i+1][j-1] != "mine":
+                        toReturn[i+1][j-1]+=1
+                    #north
+                    if checkValidIndex(d, i-1, j) and toReturn[i-1][j] != "mine":
+                        toReturn[i-1][j]+=1
+                    #south
+                    if checkValidIndex(d, i+1, j) and toReturn[i+1][j] != "mine":
+                        toReturn[i+1][j]+=1
+                    #northesat
+                    if checkValidIndex(d, i-1, j+1) and toReturn[i-1][j+1] != "mine":
+                        toReturn[i-1][j+1]+=1
+                    #east
+                    if checkValidIndex(d, i, j+1) and toReturn[i][j+1] != "mine":
+                        toReturn[i][j+1]+=1
+                    #southeast
+                    if checkValidIndex(d, i+1, j+1) and toReturn[i+1][j+1] != "mine":
+                        toReturn[i+1][j+1]+=1
+                    minesRemaining -= 1
+                    if minesRemaining == 0:
+                        cease = True
+                    if cease:
+                        break
+
+    return toReturn
+
+#fill the matrix indices with how many mines are around that index, return a list of information for each index
+def setMineNumbers(matrix):
+    pass
+
+def printMatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            print(str(matrix[i][j]) + " ", end='')
+        print("\n")
+
+printMatrix(generate(5, 5))
